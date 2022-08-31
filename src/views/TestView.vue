189888252af1dbd8df2d7633fb73f9e5dbd4f74f
Omar Qayum <omar.qayum@gmail.com>
2022-08-31 03:35:25 -0400

    Added buttons to items in cart view

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,24 +1,44 @@
 <script setup>
 import { ref } from "vue";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faMinus, faHeart } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCartShopping);
+library.add(faMinus);
+library.add(faHeart);
 </script>
 
 <template>
-<div class="nav-container">
-<h2>Movies: 5</h2>
-<h2>Movies: 5</h2>
-<h2>Movies: 5</h2>
-<h2>Movies: 5</h2>
-</div>
+  <div class="cart-container">
+    <img />
+    <div class="controls-container">
+      <button>
+        <icon class="fa-2x" icon="fa-solid fa-minus" />
+      </button>
+      <button>
+        <icon class="fa-2x" icon="fa-solid fa-heart" />
+      </button>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.nav-container {
-  display: flex;
-  gap: 20px;
-  height: 50px;
+.cart-container {
+  img {
+    height: 200px;
+    width: 150px;
+    background: grey;
+  }
+
+  .controls-container {
+    display: flex;
+    width: 150px;
+    
+    button {
+      background: $red;
+      height: 50px;
+      width: 50%;
+      border: none;
+    }
+  }
 }
 </style>
--- src/views/account/CartView.vue
+++ src/views/account/CartView.vue
@@ -1,15 +1,33 @@
 <script setup>
 import { useUserStore } from "@/store/index.js";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faMinus, faHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faMinus);
+library.add(faHeart);
 
 const userStore = useUserStore();
+
+const moveToFavourites = (category, id, record) => {
+  userStore.cart.get(category).delete(record.id);
+  // add to fav!
+}
 </script>
 
 <template>
   <h1>Shopping Cart</h1>
   <div v-for="category in ['books', 'games', 'movies', 'music']" :key="category" class="category-container">
-    <h2>{{  category  }}</h2>
+    <h2>{{ category }}</h2>
     <div class="cart-container">
-      <img v-for="[id, record] in userStore.cart.get(category)" :key="id" :src=record.posterPath @click="userStore.cart.get(category).delete(record.id)" />
+      <img v-for="[id, record] in userStore.cart.get(category)" :key="id" :src=record.posterPath />
+      <div class="controls-container">
+        <button @click="userStore.cart.get(category).delete(record.id)">
+          <icon class="fa-2x" icon="fa-solid fa-minus" />
+        </button>
+        <button @click="moveToFavourites()">
+          <icon class="fa-2x" icon="fa-solid fa-heart" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -21,12 +39,22 @@ const userStore = useUserStore();
   }
 
   .cart-container {
-    display: flex;
-
     img {
       height: 200px;
       width: 150px;
       background: grey;
+    }
+
+    .controls-container {
+      display: flex;
+      width: 150px;
+
+      button {
+        background: $red;
+        height: 50px;
+        width: 50%;
+        border: none;
+      }
     }
   }
 }
