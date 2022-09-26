f8e1fba188c563bb7442cb2f5ba369d97fc5a525
Omar Qayum <omar.qayum@gmail.com>
2022-09-25 22:04:17 -0400

    Modified register view to accomodate clamped dimensions

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { ref } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGoogle, faGithub, faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faBook, faGamepad, faFilm, faMusic } from "@fortawesome/free-solid-svg-icons";
 import { useUserStore } from "@/store/index.js";
 
+library.add(faGoogle);
+library.add(faFacebook);
+library.add(faTwitter);
+library.add(faGithub);
 library.add(faBook);
 library.add(faGamepad);
 library.add(faFilm);
@@ -21,12 +26,24 @@ const errorMessage = ref("");
 <template>
   <div class="register-outer-container">
     <div class="register-inner-container">
-      <p class="register-slogan">Your Entertainment Awaits!</p>
+      <p class="register-slogan">Register</p>
       <form @submit.prevent="userStore.register({ displayName: username, email, password, plan })">
-        <p class="form-header">Register</p>
         <input type="text" v-model="username" placeholder="Username" required />
-        <input type="email" v-model="email" placeholder="Email" required />
-        <input type="password" v-model="password" placeholder="Password" required />
+        <div class="register-email">
+          <p>Register with Email:</p>
+          <input type="email" v-model="email" placeholder="Email" required />
+          <input type="password" v-model="password" placeholder="Password" required />
+        </div>
+        <p class="register-separator">or</p>
+        <div class="register-oauth">
+          <p>Register with Provider:</p>
+          <div class="oauth-icons">
+            <icon class="oauth-icon" icon="fa-brands fa-google" />
+            <icon class="oauth-icon" icon="fa-brands fa-facebook" />
+            <icon class="oauth-icon" icon="fa-brands fa-twitter" />
+            <icon class="oauth-icon" icon="fa-brands fa-github" />
+          </div>
+        </div>
         <p v-if="errorMessage">{{ errorMessage }}</p>
         <div class="plans">
           <p class="plan-header">Plan</p>
@@ -34,7 +51,7 @@ const errorMessage = ref("");
           <icon class="plan-icon" icon="fa-solid fa-gamepad" />
           <icon class="plan-icon" icon="fa-solid fa-film" />
           <icon class="plan-icon" icon="fa-solid fa-music" />
-          <label for="bookworm">
+          <label for="bookworm" :selected="plan === 'bookworm'">
             <input
               type="radio"
               v-model="plan"
@@ -45,11 +62,11 @@ const errorMessage = ref("");
             />
             Bookworm
           </label>
-          <p class="plan-quota">5</p>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">2</p>
-          <label for="geek">
+          <p class="plan-quota" :selected="plan === 'bookworm'">5</p>
+          <p class="plan-quota" :selected="plan === 'bookworm'">2</p>
+          <p class="plan-quota" :selected="plan === 'bookworm'">2</p>
+          <p class="plan-quota" :selected="plan === 'bookworm'">2</p>
+          <label for="geek" :selected="plan === 'geek'">
             <input
               type="radio"
               v-model="plan"
@@ -60,11 +77,11 @@ const errorMessage = ref("");
             />
             Geek
           </label>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">5</p>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">2</p>
-          <label for="binger"
+          <p class="plan-quota" :selected="plan === 'geek'">2</p>
+          <p class="plan-quota" :selected="plan === 'geek'">5</p>
+          <p class="plan-quota" :selected="plan === 'geek'">2</p>
+          <p class="plan-quota" :selected="plan === 'geek'">2</p>
+          <label for="binger" :selected="plan === 'binger'"
             ><input
               type="radio"
               v-model="plan"
@@ -75,11 +92,11 @@ const errorMessage = ref("");
             />
             Binger
           </label>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">5</p>
-          <p class="plan-quota">2</p>
-          <label for="audiophile">
+          <p class="plan-quota" :selected="plan === 'binger'">2</p>
+          <p class="plan-quota" :selected="plan === 'binger'">2</p>
+          <p class="plan-quota" :selected="plan === 'binger'">5</p>
+          <p class="plan-quota" :selected="plan === 'binger'">2</p>
+          <label for="audiophile" :selected="plan === 'audiophile'">
             <input
               type="radio"
               v-model="plan"
@@ -90,10 +107,10 @@ const errorMessage = ref("");
             />
             Audiophile
           </label>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">2</p>
-          <p class="plan-quota">5</p>
+          <p class="plan-quota" :selected="plan === 'audiophile'">2</p>
+          <p class="plan-quota" :selected="plan === 'audiophile'">2</p>
+          <p class="plan-quota" :selected="plan === 'audiophile'">2</p>
+          <p class="plan-quota" :selected="plan === 'audiophile'">5</p>
         </div>
         <input type="submit" value="Register" />
       </form>
@@ -112,7 +129,8 @@ const errorMessage = ref("");
   background-color: $navyBlue;
 
   .register-inner-container {
-    width: clamp(280px, 100vw, 450px);
+    width: clamp(280px, 100vw, 500px);
+    overflow-y: auto;
 
     .register-slogan {
       font-size: 2.5rem;
@@ -128,21 +146,63 @@ const errorMessage = ref("");
       align-items: center;
       background-color: $skyBlue;
       padding: 1rem;
-      gap: 1rem;
+      gap: 0.5rem;
 
-      .form-header {
-        font-size: 2rem;
-        font-weight: 700;
-        text-align: center;
-        color: $lightBlack;
-      }
-
-      input[type="text"],
-      input[type="email"],
-      input[type="password"] {
+      input[type="text"] {
         width: 100%;
         font-size: 1.25rem;
         color: $darkBlack;
+      }
+
+      .register-email {
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        gap: 0.5rem;
+        background-color: $lightBlue;
+
+        p {
+          font-weight: 700;
+          align-self: start;
+          color: $lightBlack;
+        }
+        input {
+          width: 100%;
+          font-size: 1.25rem;
+          color: $darkBlack;
+        }
+      }
+
+      .register-separator {
+        font-weight: 700;
+        color: $lightBlack;
+      }
+
+      .register-oauth {
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        gap: 0.5rem;
+        background-color: $lightBlue;
+
+        p {
+          font-weight: 700;
+          align-self: start;
+          color: $lightBlack;
+        }
+
+        .oauth-icons {
+          display: flex;
+          justify-content: space-evenly;
+          width: 100%;
+
+          .oauth-icon {
+            font-size: 1.75rem;
+            color: red;
+          }
+        }
       }
 
       .plans {
@@ -150,7 +210,9 @@ const errorMessage = ref("");
         grid-template-columns: repeat(5, 1fr);
         justify-items: center;
         align-items: center;
-        gap: 0.75rem;
+        padding: 1rem;
+        gap: 0.5rem;
+        background-color: $lightBlue;
 
         .plan-header {
           font-size: 1.5rem;
@@ -169,7 +231,11 @@ const errorMessage = ref("");
           justify-self: start;
           font-size: 1rem;
           gap: 0.5rem;
-          color: white;
+          font-weight: bold;
+
+          &[selected="true"] {
+            color: $lightBlack;
+          }
 
           input[type="radio"] {
             height: 1rem;
@@ -179,7 +245,12 @@ const errorMessage = ref("");
 
         .plan-quota {
           font-size: 1.25rem;
+          font-weight: bold;
           color: white;
+        }
+
+        p[selected="true"] {
+          color: $lightBlack;
         }
       }
 
