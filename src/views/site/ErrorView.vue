a8439dfa8dc976da96f3f549fe5542e7dd0053be
Omar Qayum <omar.qayum@gmail.com>
2022-10-02 08:24:22 -0400

    Modified error view to accomodate clamped dimensions

--- src/views/site/ErrorView.vue
+++ src/views/site/ErrorView.vue
@@ -1,8 +1,26 @@
 <template>
-  <h1>404 Error</h1>
-  <h1>Page not found!</h1>
+  <div class="error-container">
+    <p>404 Error</p>
+    <p>Page not found!</p>
+  </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.error-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: $navyBlue;
+  gap: 0.5rem;
 
+  p {
+    font-weight: 700;
+    font-size: 3rem;
+    color: white;
+    text-align: center;
+  }
+}
 </style>
