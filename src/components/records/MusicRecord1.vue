677ad59391534fe1b0bd816d44f5da400d0e87a6
Omar Qayum <omar.qayum@gmail.com>
2022-10-09 00:13:04 -0400

    Made music searchable

---
+++ src/components/records/MusicRecord1.vue
@@ -0,0 +1,112 @@
+<script setup>
+import axios from "axios";
+import SiteTabs from "@/components/site/SiteTabs.vue";
+import RecordControls from "@/components/records/RecordControls.vue";
+import RecordAudioPlayer from "@/components/records/RecordAudioPlayer.vue";
+
+const props = defineProps(["record"]);
+const record = (await axios.get(`http://localhost:5000/api/v1/user/search/music/${props.record.id}`)).data;
+</script>
+
+<template>
+  <div class="record">
+    <SiteTabs :tabs="['about', 'tracks', 'details']" class="tabs">
+      <template #about>
+        <div class="about">
+          <img :src="record.image" />
+          <div class="details">
+            <h1 class="title">{{ record.title }}</h1>
+            <h1>{{ record.artist }}</h1>
+            <h1>{{ record.genre }}</h1>
+            <h1>{{ record.date }}</h1>
+            <h1>{{ record.totalTracks }} Tracks</h1>
+          </div>
+        </div>
+      </template>
+      <template #tracks>
+        <RecordAudioPlayer :tracks="record.tracks" />
+      </template>
+      <template #details>
+        <div class="details"></div>
+      </template>
+    </SiteTabs>
+    <RecordControls class="controls" category="music" :record="record" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.record {
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  gap: 0.5rem;
+  background-color: $lightBlack;
+
+  .tabs {
+    grid-column: span 10;
+    grid-row: 1 / span 9;
+
+    .about {
+      display: flex;
+      flex-direction: column;
+      background: $lightBlack;
+      gap: 0.5rem;
+      height: 100%;
+
+      img {
+        height: 40%;
+        aspect-ratio: 3 / 4;
+        align-self: center;
+      }
+
+      .details {
+        width: 100%;
+        line-height: 1.5rem;
+        overflow-y: auto;
+
+        h1.title {
+          font-size: 1.5rem;
+          color: $lightBlue;
+        }
+      }
+    }
+  }
+
+  .controls {
+    grid-column: span 10;
+  }
+}
+
+@media (orientation: landscape) and (min-width: 568px) {
+  .record {
+    grid-template-columns: repeat(16, 1fr);
+    grid-template-rows: repeat(16, 1fr);
+
+    .tabs {
+      grid-column: span 16;
+      grid-row: 1 / span 15;
+
+      .about {
+        height: 100%;
+        flex-direction: row;
+        align-items: center;
+
+        img {
+          height: 100%;
+        }
+
+        .details {
+          height: 100%;
+        }
+      }
+    }
+
+    .controls {
+      grid-column: span 16;
+    }
+  }
+}
+</style>
